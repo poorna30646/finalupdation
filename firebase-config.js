75705d88283dd65bad1ec5245e616ed868146075
poorna30646 <thotipoornachandra@gmail.com>
2025-12-27 10:13:27 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBQBVa6GZK1WwjmfmC1Iagvls-E9HKpEyU",
+    authDomain: "pc2web-7fd22.firebaseapp.com",
+    projectId: "pc2web-7fd22",
+    storageBucket: "pc2web-7fd22.firebasestorage.app",
+    messagingSenderId: "1067834098458",
+    appId: "1:1067834098458:web:a29bd36ff3b8df4bcd62a3"
+  };
